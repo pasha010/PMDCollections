e74f10212390bd23f66472e598fa9feb87b0b00d
Pavel Malkov <mpa026@gmail.com>
2017-06-28 18:05:19 +0300

    Initial Commit

---
+++ PMDCollections/PMDCollections.h
@@ -0,0 +1,19 @@
+//
+//  PMDCollections.h
+//  PMDCollections
+//
+//  Created by Pavel Malkov on 28.06.17.
+//  Copyright © 2017 Prime Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PMDCollections.
+FOUNDATION_EXPORT double PMDCollectionsVersionNumber;
+
+//! Project version string for PMDCollections.
+FOUNDATION_EXPORT const unsigned char PMDCollectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PMDCollections/PublicHeader.h>
+
+
---
+++ PMDCollectionsTests/PMDCollectionsTests.m
@@ -0,0 +1,39 @@
+//
+//  PMDCollectionsTests.m
+//  PMDCollectionsTests
+//
+//  Created by Pavel Malkov on 28.06.17.
+//  Copyright © 2017 Prime Digital. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PMDCollectionsTests : XCTestCase
+
+@end
+
+@implementation PMDCollectionsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
